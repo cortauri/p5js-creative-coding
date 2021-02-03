1b3c954459b30ac086ad8dacce6886f75eb7de86
Andreas Netzel <andreasnetzel@me.com>
2021-02-03 20:05:46 +0000

    some examples for drawing shapes

--- Part 1 - Get Started/sketch.js
+++ Part 1 - Get Started/sketch.js
@@ -1,7 +1,24 @@
 function setup() {
+  //Erstelle Leinwand
   createCanvas(400, 400)
 }
 
 function draw() {
-  background(0)
+  
+  //Zeichenwerkzeuge setzen
+  //Wähle die Farbe
+  background(200)
+  fill(255)
+  stroke(255,0,0)
+  
+  //Wähle die Pinselbreite
+  strokeWeight(5)
+  
+  //Shapes
+  rect(0,0,200,200)
+  circle(200,200,40)
+  ellipse(240,240,50,100)
+  line(400,400,200,200)
+  point(150,300)
+  
 }
