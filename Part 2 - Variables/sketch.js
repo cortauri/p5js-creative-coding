744835d6ca755c419e4812ffb925144d32f65ccf
Andreas Netzel <andreasnetzel@me.com>
2021-02-04 15:41:26 +0000

    Part 2, Example added

--- Part 2 - Variables/sketch.js
+++ Part 2 - Variables/sketch.js
@@ -1,8 +1,36 @@
+let farbe = 0;
+
 function setup() {
   createCanvas(400,400)
+  //Hintergrund zu Beginn erstellen
   background(200)
 }
 
 function draw() {
-  // put drawing code here
+  
+  //1. Zeichenwerkzeuge setzen
+  //Wähle die Farbe
+  //background(200);
+  fill(farbe);
+  stroke(255,0,0);
+  
+  //Wähle die Pinselbreite
+  //strokeWeight(5);
+  noStroke();
+  
+  //2. Zeichnen der Formen
+  //circle(200,200,20,20)
+    
+  if(mouseIsPressed){
+    circle(mouseX,mouseY,20,20)
+  }
+  
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    farbe = 255;
+  } else if (keyCode === RIGHT_ARROW) {
+    farbe = 0;
+  }
 }
